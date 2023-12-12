08c45c87ec529b6d97744fb977205cfe84c5b7f0
Chelsey7667 <151678580+Chelsey7667@users.noreply.github.com>
2023-12-12 18:26:21 -0500

    Add files via upload

---
+++ cprversion3.h
@@ -0,0 +1,29 @@
+#pragma once
+
+//Group Members: Mariya Thomas, Chelsey Tharayil Moni, Taran Virk, Harshit Guglani and Ali Khorram
+
+#ifndef _CONVERTING_H_
+#define _CONVERTING_H_
+#endif
+
+#ifndef _MANIPULATING_H_
+#define _MANIPULATING_H_
+#endif
+
+#ifndef _FUNDAMENTALS_H_
+#define _FUNDAMENTALS_H_
+#endif
+
+#ifndef _TOKENIZING_H_
+#define _TOKENIZING_H_
+#endif
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+void converting(void); //Converting Function Prototype
+void manipulating(void); //Manipulating Function Prototype
+void fundamentals(void); //Fundamentals Function Prototype
+void tokenizing(void); //Tokenizing Function Prototype
